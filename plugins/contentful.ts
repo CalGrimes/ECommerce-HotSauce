efbbe763dd6ad1e2a9b4526b96e6a25a238441c2
CalGrimes <cal.grimes@outlook.com>
2023-11-26 22:54:09 +0000

    Update contentful.ts

--- plugins/contentful.ts
+++ plugins/contentful.ts
@@ -1,14 +1,19 @@
-import * as contentful from "contentful";
+import { createClient } from "contentful";
+import contentful from 'contentful'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
-  const client = contentful.createClient({
+
+  const createClientFunc = process.env.NODE_ENV === 'development' ? createClient : contentful.createClient
+
+  const client = createClientFunc({
     space: config.public.contentfulSpace,
-    accessToken: config.public.contentfulPublicAccessToken,
+    accessToken: config.public.contentfulPublicAccessToken
   });
+
   return {
     provide: {
-      contentful: client,
+      contentfulClient: client,
     },
   };
 });
